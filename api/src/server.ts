a1b80712828e469c1b094c172d917a12579f3254
Laura Bassani <bassanilaurarita@gmail.com>
2020-09-19 15:29:51 +0200

    create alert message when order is completed

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
-const port = process.env.NODE_PORT || 4848;
 
+interface Item {
+  id :string
+}
+
+const port = process.env.NODE_PORT || 4848;
 
 export function run () {
   
   const app = express();   
+
+  app.use(express.json()) 
+
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -19,24 +26,34 @@ export function run () {
     database : 'menu'
   });
 
-  
-
   app.get("/", function(_, res) {
-    connection.connect(function(err: Error) {
       connection.query("SELECT * FROM menu", (err :Error, results :any, fields :any) => {
         if (err) throw err;
-        console.log(results);
         res.type('text/plain').send(results);
-      })
-    });
+    })
   });
- 
-  
 
+  app.post("/", function(req, res){
+    
+
+    connection.query("INSERT INTO purchase (pending) VALUES (TRUE)", (err :Error, results :any, fields :any) => {
+      if (err) console.log(err);
+
+      const values  = req.body.map((item :Item) => { return [item.id, results.insertId]})      
+
+      connection.query("INSERT INTO menuPurchaseLink (menuID, purchaseID) VALUES ?", [values] , (err :Error, results :any, fields :any) => {
+        if (err) throw err     
+        setTimeout(function(){ 
+          res.send()
+        }, 3000) 
+      })
+    })
+  });
+  
   return app.listen(port, function () {
-    // Port is forwarded by docker to 80.
     console.log(`Listening on http://localhost:${port}`);
   })
+
 }
 
 
